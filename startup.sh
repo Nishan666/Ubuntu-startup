b3397b9ee3e2b3ceb1a4ad432188b168a195e322
nishan666 <nishankumar559@gmail.com>
2024-09-16 12:16:45 +0530

    added brave

--- startup.sh
+++ startup.sh
@@ -258,7 +258,11 @@ rm -rf aws-sam-cli-linux-x86_64.zip sam-installation awscliv2.zip aws
 #basic folders
 mkdir 7edge personal-pro nick-test aws react react-native
 
-
+#brave browser
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install -y brave-browser
 
 
 
